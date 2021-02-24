4a8c9775e7e3674f06277dea67582300a37235bb
tonyrpark <tonyrpark@gmail.com>
2021-02-23 16:25:38 -0800

    Finished 'employee.js' and 'engineer.js' also added a .gitignore

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,26 @@
+// TODO: Write code to define and export the Employee class
+// Employee will be the parent class, other templates 'engineer, intern, manager' will be extended
+class Employee {
+  constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  getEmail() {
+    return this.email;
+  }
+
+  getRole() {
+    return "Employee";
+  }
+}
+module.exports = Employee;
