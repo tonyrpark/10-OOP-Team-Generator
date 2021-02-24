d643fb2b32034757601862f5183ef403e2f4e16d
tonyrpark <tonyrpark@gmail.com>
2021-02-24 13:42:35 -0800

    Added const values, added express code

---
+++ Develop/PSEUDOcode.md
@@ -0,0 +1,5 @@
+# UNIT 10 OOP Homework: Template Engine - Employee Summary
+
+1. Go through instructions, install packages including 'inquirer'
+2. Write the code in Employee.js
+3. Extend the code in Employee.js to the other roles
--- Develop/app.js
+++ Develop/app.js
@@ -1,14 +1,38 @@
+// Classes
+const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
+// npm
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+// Adding jest to this list to allow testing to properly occur
+const jest = require("jest");
+const express = require("express");
+const path = require("path");
+const axios = require("axios");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+var pdf = require("html-pdf");
+var options = {
+  format: "Letter",
+};
 
+// Output
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
+const generateHTML = require("./lib/generateHTML");
 const render = require("./lib/htmlRenderer");
+
+// Express App Set-up
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up Express for urlencoding (example putting %20 in place of spaces, allowing browser to read) & outputting in json format
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
